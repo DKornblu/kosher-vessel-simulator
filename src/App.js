e1f76eacc3cf48bd68a19862066301b00a17b907
DKornblu <kornbluth.david@gmail.com>
2023-11-19 22:34:45 +0200

    updated calcs, added ratio, imported chakra spinner

--- src/App.js
+++ src/App.js
@@ -1,48 +1,67 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import { ChakraProvider, Spinner } from '@chakra-ui/react';
 
 function App() {
   const [height, setHeight] = useState(0);
   const [diameter, setDiameter] = useState(0);
   const [thickness, setThickness] = useState(0);
   const [result, setResult] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // calculations
-    // volume of cylinder: V = (pi)(r^2)(h)
     // let h = height, t = thickness, r = outer radius, r-t = inner radius
-    // outer volume = (pi)(r^2)(h)
-    // inner volume = (pi)([r-t]^2)(h)
-    // wall volume = outer-inner = (pi)(h)([r^2]-[(r-t)^2])
-    // base = (pi)(r^2)(t) --> (cylinder whose height is the pot thickness)
-    // volume of metal of a pot: wall volume + base volume
-    // TotalVolume = (pi)(h)([r^2]-[(r-t)^2])+(pi)(r^2)(t)
+    // volume of cylinder: V = (pi)(r^2)(h)
+    // outer volume = (pi)(r^2)(h), inner volume = (pi)([r-t]^2)(h)
+    // Wall volume = outer vol - inner vol
+    // base volume = (pi)(r^2)(t) --> (cylinder volume whose height is the pot thickness)
+    // Total volume of metal: wall volume + base volume
 
     let pi = Math.PI;
     let t = parseFloat(thickness);
     let d = parseFloat(diameter);
     let h = parseFloat(height);
-    let r = ((d/2) + t);
+    let r = d / 2 + t; // outer radius
 
-    let outVolume = pi*(r**2)*h;
-    let inVolume = pi*((r-t)**2)*h;
+    let outVolume = pi * r ** 2 * (h - t);
+    let inVolume = pi * (r - t) ** 2 * (h - t);
     let wallVolume = outVolume - inVolume;
-    let baseVolume = pi*(r**2)*(t);
-    let totalVolume = wallVolume + baseVolume;
+    let baseVolume = pi * r ** 2 * t;
+    let totalMetalVolume = wallVolume + baseVolume;
+    let innerToMetalRatio = inVolume / totalMetalVolume;
+    let percentRatio = innerToMetalRatio * 100;
 
     console.group();
-    console.log("pi", pi)
-    console.log("height", h)
-    console.log("outer radius", r)
-    console.log("inner radius", (r-t)**2)
-    console.log("inner vol", inVolume)
-    console.log("outer vol", outVolume)
-    console.log("wall vol", wallVolume)
-    console.log("base vol", baseVolume)
-    console.log("Total vol", totalVolume)
+    console.log(
+      'height =',
+      h,
+      ', outer radius =',
+      r,
+      ', inner radius =',
+      r - t
+    );
+    console.log('inner vol =', inVolume, ', outer vol =', outVolume);
+    console.log(
+      'wall vol = (outer - inner) =',
+      wallVolume,
+      ', base vol =',
+      baseVolume
+    );
+    console.log('Total vol = wall + base =', totalMetalVolume);
+    console.log(
+      'inner volume to metal volume ratio: ',
+      innerToMetalRatio,
+      'i.e. pot contents is',
+      percentRatio,
+      '% '
+    );
     console.groupEnd();
 
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
 
     // setResult();
   };
@@ -52,47 +71,61 @@ function App() {
   // }, [height, diameter, thickness])
 
   return (
-    <div className="App">
-      <div className="input-container">
-        <form className="dimensions" onSubmit={handleSubmit}>
-          <h1>Vessel Dimensions</h1>
-          <div className="value">
-            <label htmlFor="height">Height: </label>
-            <input
-              type="number"
-              id="height"
-              value={height}
-              onChange={(e) => setHeight(e.target.value)}
-            />
-          </div>
-          <div className="value">
-            <label htmlFor="diameter">Inner diameter: </label>
-            <input
-              type="number"
-              id="diameter"
-              value={diameter}
-              onChange={(e) => setDiameter(e.target.value)}
-            />
-          </div>
-          <div className="value">
-            <label htmlFor="thickness">Wall thickness: </label>
-            <input
-              type="number"
-              id="height"
-              value={thickness}
-              onChange={(e) => setThickness(e.target.value)}
-            />
-          </div>
-        </form>
+    <ChakraProvider>
+      <div className="App">
+        <div className="input-container">
+          <form className="dimensions" onSubmit={handleSubmit}>
+            <h1>Vessel Dimensions</h1>
+            <div className="value">
+              <label htmlFor="height">Height: </label>
+              <input
+                type="number"
+                id="height"
+                value={height}
+                onChange={(e) => setHeight(e.target.value)}
+              />
+            </div>
+            <div className="value">
+              <label htmlFor="diameter">Inner diameter: </label>
+              <input
+                type="number"
+                id="diameter"
+                value={diameter}
+                onChange={(e) => setDiameter(e.target.value)}
+              />
+            </div>
+            <div className="value">
+              <label htmlFor="thickness">Wall thickness: </label>
+              <input
+                type="number"
+                id="height"
+                value={thickness}
+                onChange={(e) => setThickness(e.target.value)}
+              />
+            </div>
+          </form>
 
-        <button onClick={handleSubmit}>Calculate</button>
-      </div>
+          <button onClick={handleSubmit}>Calculate</button>
+        </div>
 
-      <div className="result-container">
-        <label>Result: </label>
-        <>{result}</>
+        <div>
+          {isLoading ? (
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+            />
+          ) : null}
+        </div>
+
+        <div className="result-container">
+          <label>Result: </label>
+          <>{result}</>
+        </div>
       </div>
-    </div>
+    </ChakraProvider>
   );
 }
 
